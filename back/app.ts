1bede0a650a9c05750c103b1889b05662d09f401
eikort <eino@weasel.fi>
2021-06-08 23:09:24 +0300

    added stuff

--- back/app.ts
+++ back/app.ts
@@ -1,19 +1,30 @@
 import express from 'express';
 
 const app = express();
+
 const cors = require('cors')
 const bodyParser = require('body-parser')
+const jsonParser = bodyParser.json()
+
 const port = 3001;
 const db = require('./queries')
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
 app.use(cors())
-app.use(bodyParser.json())
 app.listen(port, () => {
   console.log(`application is running on port ${port}.`);
 });
 
-app.get("/api/times", (req, res, next) => {
+app.get("/api/times/:calendarValue", (req, res, next) => {
 
-  res.json([{time:'8-9', taken:false, index:1}, {time:'9-10', taken:false, index:2},{time:'10-11', taken:false, index:3},{time:'11-12', taken:false, index:4},{time:'12-13', taken:false, index:5},{time:'13-14', taken:false, index:6}, {time:'14-15', taken:false, index:7}, {time:'15-16', taken:false, index:8}]);
+  db.getTimesByCalendarValue(req,res)
+ });
+ app.get("/api/times", (req, res, next) => {
+
+  db.getTimes(req,res)
  });
  
-app.post("/api/times/:calendarValue", db.createTimes)
+app.post('/api/times', (req,res) => {
+  
+  db.createTimes(req, res)
+})
--- back/queries.ts
+++ back/queries.ts
@@ -1,3 +1,5 @@
+import { response } from "express"
+
 const Pool = require('pg').Pool
 const pool = new Pool({
     user: 'user',
@@ -7,29 +9,42 @@ const pool = new Pool({
     port: 5432
 })
 const getTimesByCalendarValue = (req, res) => {
-
-    const id = parseInt(req.params.calendarValue)
-    pool.query('SELECT * FROM times WHERE calendarValue = $1', [req.params.calendarValue], (error, results) => {
+    console.log(JSON.stringify(req.params.calendarValue))
+    pool.query('SELECT * FROM times WHERE calendarValue = $1', [JSON.stringify(req.params.calendarValue)], (error, results) => {
         if (error) {
             throw error
         }
-        res.status(200).json(results.rows)
+        console.log(results.rows)
+        res.send(results.rows)
     }) 
+    
+}
+const getTimes = (req, res) => {
+
+    pool.query('SELECT * FROM times', (error, results) => {
+        if (error) {
+            throw error
+        }
+        console.log(results.rows)
+        res.send(results.rows)
+    })
 }
 const createTimes = (req, res) => {
     
-    let { calendarValue, times} = req.body
-    calendarValue = JSON.stringify(calendarValue)
-    times = JSON.stringify(times)
-    pool.query('INSERT INTO times (calendarValue, times) VALUES ($1, $2)', [calendarValue, times], (error,results) => {
+    const values = {calendarValue: JSON.stringify(req.body.calendarValue), times : JSON.stringify(req.body.times)}
+    
+    console.log("times: ", req.body.times)
+    pool.query('INSERT INTO times (calendarValue, times1) VALUES ($1, $2)', [values.calendarValue, values.times], (error,results) => {
         if (error) {
+            console.log(error)
             throw error
         }
-        res.status(201).send(`Times added with ID: ${res.insertId}`)
+        res.status(201).send(`Times with key ${values.calendarValue}`)
     })
 }
 
 module.exports = {
     getTimesByCalendarValue,
-    createTimes
+    createTimes,
+    getTimes
 }
